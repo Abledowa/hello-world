d5d3f712a0a116b55e9bbc196c9d921ff56e4813
Agnieszka Błędowska <agnieszka.bledowska@vazco.eu>
2018-05-10 11:08:11 +0200

    button showing coffee type

---
+++ myapp.js
@@ -0,0 +1,20 @@
+var num = 10;
+
+console.log(num);
+
+function makeCoffee(sugar, milk)
+{
+    var instructions = "Boil water,";
+    instructions += " pour into cup,";
+    instructions += " add coffee granules,";
+    instructions += " add " + sugar + " spoons of sugar,";
+    instructions += " add " + milk + "% milk.";
+
+    return instructions;
+};
+
+
+
+
+
+
